addf10ef69a76110725fa9a7b0bc6fc5795a2a10
dinodtharinda <dinodtharinda2001@gmail.com>
2023-05-02 12:31:49 +0530

    hello

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: const Text("THis is Messages!"),
+          );
+        },
+      ),
+    );
+  }
+}
